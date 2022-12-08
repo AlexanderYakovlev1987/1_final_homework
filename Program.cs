377a3a40d1f58bcb3ef157986ed0ea7f56611a18
AlexanderYakovlev <Morganwolf87@gmail.com>
2022-12-08 21:43:47 +0300

    создал программу

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+void FillingArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+string[] sourceArray = { "123", "Length", "23", "098", "hello", "res", "world", "yes", "dzo", ":-)"};
+string[] resultingArray = new string[sourceArray.Length];
+FillingArray(sourceArray, resultingArray);
+resultingArray = resultingArray.Where(x => x != null).ToArray();
+Console.WriteLine($"\n" + '[' + string.Join(", ", sourceArray) + ("] -> [") + string.Join(", ", resultingArray) + ']' + "\n\n");
